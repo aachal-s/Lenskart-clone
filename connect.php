1c288b1c37bdfc240520ee4cf5ed99f3316ca1ee
Aachal Saxena <90494424+aachal-s@users.noreply.github.com>
2023-05-07 15:08:25 +0530

    Add files via upload
    This website is under construction.

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+    $Username = $_POST['Username'];
+    $Password = $_POST['Password'];
+    $Username = $_POST['Username'];
+    $Email_ID = $_POST['Email_ID'];
+    $Password = $_POST['Password'];
+?>
